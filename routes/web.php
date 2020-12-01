559134b41b57fa7bd3bf7d04e6a17ffcb7fbd92e
bunmin <feri.suryanata@gmail.com>
2020-12-01 15:18:49 +0700

    add laravel ui

--- routes/web.php
+++ routes/web.php
@@ -13,33 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', function() {
-    $crawler = Goutte::request('GET', 'https://duckduckgo.com/html/?q=Laravel');
-    $crawler->filter('.result__title .result__a')->each(function ($node) {
-      dump($node->text());
-    });
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/idx', function() {
-    $date = '20201126';
-    $recordsTotal = 1;
-    $url = "https://www.idx.co.id/umbraco/Surface/TradingSummary/GetStockSummary?date=$date&length=$recordsTotal";
-    $response = Http::get($url);
-    if ($response->json() == null) {
-        dd("haiya"); //nanti taruh notif yak
-    };
+Auth::routes();
 
-    $recordsTotal = $response->json()['recordsTotal'];
-    if ($recordsTotal <= 0) {
-        dd("haiya"); //nanti taruh notif yak
-    };
-
-    $url = "https://www.idx.co.id/umbraco/Surface/TradingSummary/GetStockSummary?date=$date&length=$recordsTotal";
-    $response = Http::get($url);
-    dd($response->json());
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
